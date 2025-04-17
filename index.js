c7e74142d3d46ea97be4a26e63974e841340f6aa
+ <+>
2025-04-17 14:02:33 +1000

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const nameList = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(array, message) {
+    const newNameList = []
+    for (let i = 0; i < array.length; i++) {
+        newNameList.push(`Thank you, ${array[i]}, for the wonderful ${message} gift!`);
+        debugger;
+    }
+
+    return newNameList;
+}
+
+writeCards(nameList, "surprise");
+
+function countDown(n){
+    let count = 0
+    while(count <= n){
+        console.log(n--);
+        debugger; 
+    }
+
+}
+
+countDown(10);
